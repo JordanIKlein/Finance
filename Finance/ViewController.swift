ef7c05ebd943f296390da47a1cf5d7830b874f10
Jordan Klein <jordaniklein03@gmail.com>
2020-11-21 21:00:00 -0500

    Initial Commit

---
+++ Finance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Finance
+//
+//  Created by Jordan Klein on 11/21/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
